484395d2abe9ed5f2d8e1dccb62427eda59e41c7
kurnia123 <rizkykurnia219@gmail.com>
2021-05-21 09:04:47 +0700

    menghapus host pada Hapi.server

--- src/server.js
+++ src/server.js
@@ -4,8 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         // port: 5000,
-        port: process.env.PORT || 5000,
-        host: '0.0.0.0',
+        port: ~~process.env.PORT || 5000,
         routes: {
             cors: {
                 origin: ['*'],
